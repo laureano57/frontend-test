6632a823a9b1d9f6394fed32ca913b63c360c0f3
laureanotodomal <laureano.57@gmail.com>
2019-01-30 21:14:51 -0300

    Add banner and js slider

---
+++ scripts/banner.js
@@ -0,0 +1,26 @@
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("banner-slide");
+  var dots = document.getElementsByClassName("dot");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" dot-white", "");
+  }
+  x[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " dot-white";
+}
